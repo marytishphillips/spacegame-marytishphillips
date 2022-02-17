07d63fb86eea72e80c72958314b4411ae55a8871
Mary Tish Phillips <marytishphillips@Marys-MacBook-Pro.local>
2022-02-17 15:59:02 -0600

    Made changes to volume and vec2 classes

--- src/main/scala/cs2/units/Volume.scala
+++ src/main/scala/cs2/units/Volume.scala
@@ -4,42 +4,60 @@ class Volume(private var lit:Double = 0.0) {
   //Field is in the argument list above - the volume stored in LITERS
 
   //Basic math operators to add, subtract, and scale volumes
-  def + (other:Volume):Volume = { ??? }
-  def += (other:Volume):Unit  = { ??? }
-
-  def - (other:Volume):Volume = { ??? }
-  def -= (other:Volume):Unit  = { ??? }
-
-  def * (scalar:Double):Volume = { ??? }
-  def *= (scalar:Double):Unit  = { ??? }
-
-  def / (scalar:Double):Volume = { ??? }
-  def /= (scalar:Double):Unit  = { ??? }
+  def + (other:Volume):Volume = {
+    new Volume(this.lit + other.lit) 
+  }
+  def += (other:Volume):Unit  = {
+    this.lit += other.lit
+  }
+  def - (other:Volume):Volume = {
+    new Volume(this.lit - other.lit) 
+  }
+  def -= (other:Volume):Unit  = {
+    this.lit -= other.lit
+  }
+  def * (scalar:Double):Volume = {
+    new Volume(this.lit * scalar)
+  }
+  def *= (scalar:Double):Unit  = {
+    this.lit *= scalar
+  }
+  def / (scalar:Double):Volume = {
+    new Volume(this.lit / scalar)
+  }
+  def /= (scalar:Double):Unit  = {
+    this.lit /= scalar
+  }
 
   //Getter functions that return in a variety of units
-  def liters():Double = { ??? }
-  def milliliters():Double = { ??? }
-  def gallons():Double = { ??? }
-  def quarts():Double = { ??? }
-  def pints():Double = { ??? }
-  def cups():Double = { ??? }
-  def teaspoons():Double = { ??? }
-  def tablespoons():Double = { ??? }
+  def liters():Double = lit
+  def milliliters():Double = lit * 1000
+  def gallons():Double = lit * 0.264172
+  def quarts():Double = lit * 1.0567
+  def pints():Double = lit * 2.11338
+  def cups():Double = lit * 4.16667
+  def teaspoons():Double = lit * 168.936
+  def tablespoons():Double = lit * 56.3121
 }
 
 object Volume {
   //"Constructor" apply methods operating in liters
-  def apply():Volume = { ??? }
-  def apply(amt:Double):Volume = { ??? }
+  def apply():Volume = new Volume()
+  def apply(amt:Double):Volume = new Volume(amt)
 
   //Alternative "static" methods to create volumes in other units
-  def liters(amt:Double):Volume = { ??? } //identical to an apply method
-  def milliliters(amt:Double):Volume = { ??? }
-  def gallons(amt:Double):Volume = { ??? }
-  def quarts(amt:Double):Volume = { ??? }
-  def pints(amt:Double):Volume = { ??? }
-  def cups(amt:Double):Volume = { ??? }
-  def teaspoons(amt:Double):Volume = { ??? }
-  def tablespoons(amt:Double):Volume = { ??? }
+  def liters(amt:Double):Volume = { new Volume(amt) }
+  def milliliters(amt:Double):Volume = { new Volume(amt / 1000) }
+  def gallons(amt:Double):Volume = { new Volume(amt * 3.78541) }
+  def quarts(amt:Double):Volume = { new Volume(amt * 0.946453) }
+  def pints(amt:Double):Volume = { new Volume(amt * 0.473176) }
+  def cups(amt:Double):Volume = { new Volume(amt * 0.24) }
+  def teaspoons(amt:Double):Volume = { new Volume(amt * 0.00492892) }
+  def tablespoons(amt:Double):Volume = { new Volume(amt * 0.0147868) }
+
+  def main(args:Array[String]):Unit = {
+    val x = Volume(4.0)
+    println(x.gallons)
+  }
 }
 
--- src/main/scala/cs2/util/Vec2.scala
+++ src/main/scala/cs2/util/Vec2.scala
@@ -5,37 +5,49 @@ class Vec2 (var x:Double, var y:Double) {
   override def toString():String = "("+x+","+y+")"
   
   //Methods for addition and subtraction of vectors
-  def +  (other:Vec2):Vec2 = { ??? }
-  def += (other:Vec2):Unit = { ??? }
+  def +  (other:Vec2):Vec2 = { new Vec2 (this.x + other.x , this.y + other.y) }
+  def += (other:Vec2):Unit = {
+    this.x += other.x
+    this.y += other.y
+  }
   
-  def -  (other:Vec2):Vec2 = { ??? }
-  def -= (other:Vec2):Unit = { ??? }
+  def -  (other:Vec2):Vec2 = { new Vec2 (this.x - other.x, this.y - other.y)}
+  def -= (other:Vec2):Unit = {
+    this.x -= other.x
+    this.y -= other.y
+  }
 
   //Methods for multiplication and division of vectors by a scalar (non-vector)
-  def *  (scalar:Double):Vec2 = { ??? }
-  def *= (scalar:Double):Unit = { ??? }
+  def *  (scalar:Double):Vec2 = { new Vec2 (this.x * scalar, this.y * scalar) }
+  def *= (scalar:Double):Unit = {
+    this.x *= scalar
+    this.y *= scalar
+  }
 
-  def /  (scalar:Double):Vec2 = { ??? }
-  def /= (scalar:Double):Unit = { ??? }
+  def /  (scalar:Double):Vec2 = { new Vec2 (this.x / scalar, this.y / scalar) }
+  def /= (scalar:Double):Unit = {
+    this.x /= scalar
+    this.y /= scalar
+  }
 
   //Methods to determine the length of a vector (magnitude and length should return the same value)
-  def magnitude():Double = { ??? }
-  def length():Double = { ??? }
+  def magnitude():Double = { math.sqrt(math.pow(x, 2) + math.pow(y, 2)) }
+  def length():Double = { magnitude() }
   
   //Methods to calculate the dot product (same returns)
-  def dot(other:Vec2):Double = { ??? }
-  def **(other:Vec2):Double = { ??? }
+  def dot(other:Vec2):Double = { (this.x * other.x) + (this.y * other.x) }
+  def **(other:Vec2):Double = { dot(other) }
   
   //Methods to determine the angle between 2 vectors (same returns)
-  def angleBetween(other:Vec2):Double = { ??? }
-  def <>(other:Vec2):Double = { ??? }
+  def angleBetween(other:Vec2):Double = { math.asin(dot(other)/(magnitude()*(math.sqrt(math.pow(other.x, 2) + math.pow(other.y, 2))))) }
+  def <>(other:Vec2):Double = { angleBetween(other) }
 
   //Methods to return a new vector that is in the same direction, but length 1 (same returns)
-  def normalize():Vec2 = { ??? }
-  def unary_~ : Vec2 = { ??? }
+  def normalize():Vec2 = { new Vec2 ( x / magnitude(), y / magnitude()) }
+  def unary_~ : Vec2 = { normalize() }
 
   //A clone operator can be useful when making "deep" copies of objects
-  override def clone():Vec2 = { ??? }
+  override def clone():Vec2 = { new Vec2 (x , y) }
 }
 
 object Vec2 {
@@ -46,6 +58,12 @@ object Vec2 {
   def apply():Vec2 = { new Vec2(0, 0) }
 
   def main(args:Array[String]):Unit = {
+    val v1 = Vec2(29.4241, 98.4936)
+    val v2 = Vec2(30.2672, 97.7431)
+
+    val newx = ()
+    val newy = (((v1.y - v2.y)/(v1.x - v2.x)))
+    println("The drone will be at degrees latitude and degrees longitude." )
     /** Your solution to the physics problem described should be calculated here.
      *  Remember to print out your answer using println.
      */
