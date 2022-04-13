42b324ff399ef060f58ed925b32bb7d9027a6de9
Mary Tish Phillips <marytishphillips@Marys-MacBook-Pro.local>
2022-04-12 23:05:05 -0500

    Changes made to SpaceGameApp.scala

--- src/main/scala/cs2/game/SpaceGameApp.scala
+++ src/main/scala/cs2/game/SpaceGameApp.scala
@@ -14,6 +14,11 @@ import scalafx.scene.input.KeyCode
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.Buffer
 import javafx.scene.input
+import scalafx.scene.control.Button
+import scalafx.event.ActionEvent
+import scalafx.scene.text.Font
+import java.awt.Desktop.Action
+import java.awt.RenderingHints.Key
 
 object SpaceGameApp extends JFXApp {    
   stage = new JFXApp.PrimaryStage {
@@ -33,6 +38,18 @@ object SpaceGameApp extends JFXApp {
           val playBullImg = new Image(playBullImgPath.toString)
           val enemyImgPath = getClass().getResource("/images/Enemy.png")
           val enemyImg = new Image(enemyImgPath.toString)
+          val titleImgPath = getClass().getResource("/images/title.png")
+          val titleImg = new Image(titleImgPath.toString)
+          val creatorImgPath = getClass().getResource("/images/CreatedBy.png")
+          val creatorImg = new Image(creatorImgPath.toString)
+          val keyImgPath = getClass().getResource("/images/KeyToBegin.png")
+          val keyImg = new Image(keyImgPath.toString)
+          val optionsImgPath = getClass().getResource("/images/Options.png")
+          val optionsImg = new Image(optionsImgPath.toString)
+          val finalScoreImgPath = getClass().getResource("/images/FinalScore.png")
+          val finalScoreImg = new Image(finalScoreImgPath.toString)
+          val gameOverImgPath = getClass().getResource("/images/gameOver.png")
+          val gameOverImg = new Image(gameOverImgPath.toString)
 
           
           var es = new EnemySwarm(6,3)
@@ -43,12 +60,49 @@ object SpaceGameApp extends JFXApp {
           val keySet = scala.collection.mutable.Set[KeyCode]() 
           var count = 0 
           var bulletCount = 0 
+          var starterPage = true
+          var playerLives = 4
+          var playerScore = 0 
 
           val timer = AnimationTimer(t => {
-              g.drawImage(backImg, 0,0, width.value,height.value)
+            g.drawImage(backImg, 0,0, width.value,height.value)   
+            if (starterPage == true) { 
+              playerScore = 0 //added
+              g.drawImage(titleImg, 2,100, 800, 200)
+              g.drawImage(keyImg, 125, 300)
+              g.drawImage(creatorImg, 500,700)
+              for (s <- keySet) {
+                if(s == KeyCode.Y) { 
+                  starterPage = false 
+                  playerLives = 4
+                  es = new EnemySwarm(6,3)
+                }
+              }
+            }
+            else if (playerLives == 0) { 
+              g.drawImage(gameOverImg, 140, 100, 500, 200)
+              g.drawImage(finalScoreImg, 160, 300)
+              g.setFill(Color.White)
+              g.font = new Font("Impact", 150)
+              g.fillText("" + playerScore, 275, 550)
+              g.drawImage(optionsImg, 50, 600, 700, 200)
+              for(s <- keySet) {
+                if (s == KeyCode.X) starterPage = true 
+                else if (s == KeyCode.R) {
+                  playerLives = 4
+                  playerScore = 0 //added
+                  bulletList.clear()
+                  es = new EnemySwarm(6,3)
+                }
+              }
+            }
+            else {
+              g.setFill(Color.White)
+              g.font = new Font("Impact", 30)
+              g.fillText("Player Lives Remaining: " + playerLives + " / 4" , 450, 40)
+              g.fillText("Score: " + playerScore, 5, 40)
               pl.display(g)
-
-              if(es.swarm.length == 0) es = new EnemySwarm(6,3) 
+              if(es.swarm.length == 0) es = new EnemySwarm(6,3)
               es.display(g)
 
               for(x <- bulletList) { 
@@ -81,16 +135,16 @@ object SpaceGameApp extends JFXApp {
                   }
                   else bulletCount += 1
                 } 
-                else if (e == KeyCode.Left) {
+                else if (e == KeyCode.Left || e == KeyCode.A) {
                   pl.moveLeft() 
                 }
-                else if (e == KeyCode.Right) {
+                else if (e == KeyCode.Right || e == KeyCode.D) {
                   pl.moveRight()
                 } 
-                else if (e == KeyCode.Up) {
+                else if (e == KeyCode.Up || e == KeyCode.W) {
                   pl.moveUp()
                 }
-                else if (e == KeyCode.Down) {
+                else if (e == KeyCode.Down || e == KeyCode.S) {
                   pl.moveDown()
                 }
               }
@@ -101,6 +155,7 @@ object SpaceGameApp extends JFXApp {
               for (b <- enemyBullList) { 
                   if (pl.intersection(b, b.bulletImgSize, b.bulletImgSize)) { 
                     pl.playerUpdate(350, 500)
+                    playerLives -= 1
                     enemyBullDelete ::= b
                   }
               }
@@ -122,6 +177,7 @@ object SpaceGameApp extends JFXApp {
                 for (u <- bulletList) {
                   if (b.intersection(u, u.bulletImgSize, u.bulletImgSize)) {
                     enemyDelete ::= b
+                    playerScore += 100
                     bullDelete ::= u 
                   }
                 }
@@ -130,16 +186,18 @@ object SpaceGameApp extends JFXApp {
               for (e <- enemyDelete) es.swarm -= e
               
               for (t <- enemyBullDelete) enemyBullList -= t
+            }
         })
           timer.start()
+
           
           canvas.onKeyPressed = (w:KeyEvent) => {
-              if(w.code == KeyCode.Space || w.code == KeyCode.Left || w.code == KeyCode.Right || w.code == KeyCode.Up || w.code == KeyCode.Down) {
+              if(w.code == KeyCode.Space || w.code == KeyCode.S|| w.code == KeyCode.Left || w.code == KeyCode.Right || w.code == KeyCode.Up || w.code == KeyCode.Down || w.code == KeyCode.W || w.code == KeyCode.A || w.code == KeyCode.R || w.code == KeyCode.D|| w.code == KeyCode.X|| w.code == KeyCode.Y) {
                 keySet.add(w.code)
               }
           }
           canvas.onKeyReleased = (c:KeyEvent) => {
-            if(c.code == KeyCode.Space || c.code == KeyCode.Left || c.code == KeyCode.Right || c.code  == KeyCode.Up || c.code == KeyCode.Down) {
+            if(c.code == KeyCode.Space || c.code == KeyCode.S || c.code == KeyCode.Left || c.code == KeyCode.Right || c.code  == KeyCode.Up || c.code == KeyCode.Down|| c.code == KeyCode.W || c.code == KeyCode.A || c.code == KeyCode.R || c.code == KeyCode.D|| c.code == KeyCode.X|| c.code == KeyCode.Y) {
                 keySet.remove(c.code)
               }
               if(c.code == KeyCode.Space) bulletCount = 0
